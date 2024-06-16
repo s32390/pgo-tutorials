475ff67f459b380111d783d055b2e9e587eed8a4
s32390 <166310134+s32390@users.noreply.github.com>
2024-06-16 16:13:24 +0200

    Delete pgo 6 directory

--- pgo 6/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Car car = new Car("Toyota", "Corolla", 2020, 500, 20);
-        Truck truck = new Truck("Ford", "F-150", 2018, 300, 25, 2);
-
-        System.out.println("Efektywność paliwowa samochodu: " + car.calculateFuelEfficiency() + " mpg");
-        System.out.println("Efektywność paliwowa ciężarówki: " + truck.calculateFuelEfficiency() + " mpg");
-    }
-}
